5477895753e89555eb4198ac7554377b8b01555c
Ese Aigbedion <111074394+OgaGrace@users.noreply.github.com>
2025-12-28 17:47:29 +0000

    Add application files

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+import os
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def health():
+    return jsonify(
+        status="ok",
+        service="incident-sim-api"
+    )
+
+@app.route("/slow")
+def slow():
+    time.sleep(5)
+    return jsonify(message="This response was slow")
+
+@app.route("/error")
+def error():
+    if os.getenv("FAIL_MODE") == "true":
+        raise Exception("Simulated production failure")
+    return jsonify(message="No errors detected")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
